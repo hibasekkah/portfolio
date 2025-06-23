14d671241f50605bb49973d861530d0c27bc9474
hibasekkah <157165724+hibasekkah@users.noreply.github.com>
2025-06-23 23:32:41 +0100

    test

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+export const Home = () =>{
+    return ( 
+        <div className="min-h-screen">
+        {/* theme Toggle */}
+
+
+        {/* backgroud effects */}
+
+
+        {/* navbar */}
+
+
+        {/* main content */}
+
+
+        {/* footer */}
+    </div>
+    );
+}
