99207bf2429c9786c464083d355ad3188c9e6050
hibasekkah <157165724+hibasekkah@users.noreply.github.com>
2025-07-03 07:43:57 +0100

    version 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import { ThemeToggle } from "@/components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "@/components/Navbar";
+
 export const Home = () =>{
     return ( 
-        <div className="min-h-screen">
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* theme Toggle */}
-
+        <ThemeToggle />
 
         {/* backgroud effects */}
-
+        <StarBackground />
 
         {/* navbar */}
-
+        <Navbar />
 
         {/* main content */}
 
@@ -16,4 +20,4 @@ export const Home = () =>{
         {/* footer */}
     </div>
     );
-}
+};
